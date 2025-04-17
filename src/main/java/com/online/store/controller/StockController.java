d57a0176d020c15eff5e2bc6026bacfa533ad9a0
Rahul Gubiligari <rahul.gubiligari@redpincompany.com>
2025-04-17 08:57:27 +0530

    Add logging

--- src/main/java/com/online/store/controller/StockController.java
+++ src/main/java/com/online/store/controller/StockController.java
@@ -4,6 +4,7 @@ import com.online.store.application.StockApplication;
 import com.online.store.dto.StockRequest;
 import com.online.store.util.JsonResponseUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -15,12 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/stock")
 @RequiredArgsConstructor
+@Slf4j
 public class StockController {
 
     private final StockApplication stockApplication;
 
     @PostMapping("/addStock")
     public ResponseEntity<String> addStock(@RequestBody final StockRequest stockRequest) {
+        log.info("Received request to add stock: {}", stockRequest.productId());
         var addedStock = stockApplication.addStockDetails(stockRequest);
         final String jsonString = JsonResponseUtil.createJsonString(addedStock);
         return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.APPLICATION_JSON).body(jsonString);
